12834a255fd8fb293f0b6f3a707ab3dc4d36a83d
gablaxy <32848524+gablaxy@users.noreply.github.com>
2021-11-22 14:20:04 +0100

    update pc portable vrai version

--- Yams.c
+++ Yams.c
@@ -2,29 +2,85 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
 
-#define JET 5;
-typedef int jet[JET];
+#define JET 5
+#define LIGNES 17
+typedef int tab_jet[JET];
+typedef char *feuille[LIGNES];
 
-void demande_nom(char j1[20], char j2[20]){
-    printf("nom joueur 1 ?\n");
-    scanf("%s", j1);
-    printf("nom joueur 2 ?\n");
-    scanf("%s", j2);
+int jet_un_de(){
+    srand(time(NULL));
+    return rand() % 6+1;
 }
 
-void somme_des(jet jet_des, int somme_des){
-    somme_des = 0;
-    for(i=1, i<JET, i++){
-        somme_des = somme_des + i; //rajoute i à la somme des dés
+void jet(tab_jet resultats){
+    int i;
+    for(i=0; i<JET; i++){
+        sleep(1);
+        resultats[i] = jet_un_de();
+        
+    }
+}
+
+void formater_nom(char j[15]){
+    int nb_caract = 0;
+    for(int i=0;j[i];i++){
+        nb_caract++;
+    }
+    for(int i=nb_caract;i<15;i++){
+        j[i]=' ';
+    }
+}
+
+void demande_nom(char j1[15], char j2[15]){
+    printf("nom joueur 1 ?\n");
+    scanf("%s", j1);
+    formater_nom(j1);
+    printf("nom joueur 2 ?\n");
+    scanf("%s", j2);
+    formater_nom(j2);
+    printf("%s %s\n",j1,j2);
+}
+
+int somme(tab_jet jet){
+    int somme = 0;
+    int i;
+    for(i=0; i<JET; i++){
+        somme = somme + jet[i]; //rajoute i à la somme des dés
+    }
+    return somme;
+}
+
+void affiche_jet(tab_jet jet){
+    int i;
+    for(i=0; i<JET; i++){
+        printf("%d : %d\n", i, jet[i]);
+    }
+}
+
+void affiche_tab(feuille feuille_score, feuille joueur1, feuille joueur2,char j1[15], char j2[15]){
+    int i;
+    printf("Scores               | %s | %s\n",j1,j2);
+    for(i=0; i<LIGNES; i++){
+        printf("%s |    [%s]    |    [%s]\n", feuille_score[i], joueur1[i], joueur2[i]);
+    }
+}
+
+void initialisation_score(feuille joueur1, feuille joueur2){
+    int i;
+    for(i=0; i<LIGNES; i++){
+        joueur1[i] = "";
+        joueur2[i] = "";
     }
 }
 
 bool menufin(){
     int val;
+    printf("Choisissez :\n");
     printf("1 - Relancer une partie\n");
     printf("2 - Quitter\n");
-    
     scanf("%d", &val);
     while(val < 1 || val > 2){
         printf("Erreur, rentrez 1 ou 2\n");
@@ -38,7 +94,44 @@ bool menufin(){
 }
 
 int main(){
+    feuille feuille_score1, feuille_score2;
+    feuille combinaisons = {
+        "1 (total de 1)      ",
+        "2 (total de 2)      ",
+        "3 (total de 3)      ",
+        "4 (total de 4)      ",
+        "5 (total de 5)      ",
+        "6 (total de 6)      ",
+        "Bonus si > à 62 [35]",
+        "Total supérieur     ",
+        "Brelan [total]      ",
+        "Carré [total]       ",
+        "Full House [25]     ",
+        "Petite suite [30]   ",
+        "Grande suite [40]   ",
+        "Yams [50]           ",
+        "Chance [total]      ",
+        "Total inférieur     ",
+        "Total               " 
+        };
+    bool partie = true;
+    tab_jet jet_courant;
+    int somme_des = 0;
     char nom_joueur1[20], nom_joueur2[20];
-    demande_nom(nom_joueur1,nom_joueur2);
+
+    while(partie == true){
+        demande_nom(nom_joueur1,nom_joueur2);
+        
+        
+        jet(jet_courant);
+        somme_des = somme(jet_courant);
+        affiche_jet(jet_courant);
+        printf("somme : %d\n", somme_des);
+
+    
+        initialisation_score(feuille_score1, feuille_score2);
+        affiche_tab(combinaisons, feuille_score1, feuille_score2, nom_joueur1, nom_joueur2);
+        partie = menufin();
+    }
     return EXIT_SUCCESS;
 }
